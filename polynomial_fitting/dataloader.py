9e3b13688be7290d18feb0d7d45230196ca4ced2
Ritvik Gupta <ritvikgupta199@gmail.com>
2021-09-25 01:49:24 +0530

    Added Normalisation

--- polynomial_fitting/dataloader.py
+++ polynomial_fitting/dataloader.py
@@ -5,6 +5,7 @@ class DataLoader:
         self.file_path = file_path
         self.poly_deg = poly_deg
         self.train_input, self.train_target, self.test_input, self.test_target = self.load_data(split)
+        self.mu, self.sigma = self.normalise_data()        
         self.batch_size = len(self.train_input) if batch_size == -1 else batch_size
 
     def load_data(self, split):
@@ -14,24 +15,31 @@ class DataLoader:
         split = int(split*len(lines))
         for line in lines:
             nums = line.strip().split(',')
-            target.append(float(nums[0]))
-            input_x.append(float(nums[0]))
+            x = float(nums[0])**np.arange(self.poly_deg+1)
+            input_x.append(x)
+            target.append(float(nums[1]))
         train_input, train_target = np.array(input_x[:split]), np.array(target[:split])
         test_input, test_target = np.array(input_x[split:]), np.array(target[split:])
         return train_input, train_target, test_input, test_target
+    
+    def normalise_data(self):
+        mu = self.train_input.mean(0)
+        sigma = np.sqrt(np.square((self.train_input-mu)).mean(0))
+        mu[0], sigma[0] = 0.0, 1.0
+        self.train_input = (self.train_input-mu)/sigma
+        self.test_input = (self.test_input-mu)/sigma
+        return mu, sigma
 
     def get_data_queue(self, train = True):
         data_queue = []
         if train:
             for i in range(0, len(self.train_input), self.batch_size):
                 x = self.train_input[i:i+self.batch_size]
-                x = x.reshape(-1,1).repeat(self.poly_deg+1, 1)**np.arange(self.poly_deg+1).reshape(1,-1)
                 y = self.train_target[i:i+self.batch_size]
                 data_queue.append((x,y))
         else:
-            for i in range(0, len(self.train_input), self.batch_size):
+            for i in range(0, len(self.test_input), self.batch_size):
                 x = self.test_input[i:i+self.batch_size]
-                x = x.reshape(-1,1).repeat(self.poly_deg+1, 1)**np.arange(self.poly_deg+1).reshape(1,-1)
                 y = self.test_target[i:i+self.batch_size]
                 data_queue.append((x,y))
         return data_queue
--- polynomial_fitting/train.py
+++ polynomial_fitting/train.py
@@ -2,22 +2,37 @@ import numpy as np
 from .dataloader import DataLoader
 from .model import PolynomialFitter
 
+class AverageMeter(object):
+  def __init__(self):
+    self.reset()
+
+  def reset(self):
+    self.avg = 0
+    self.sum = 0
+    self.cnt = 0
+
+  def update(self, val, n=1):
+    self.sum += val * n
+    self.cnt += n
+    self.avg = self.sum / self.cnt
+
 def fit_polynomial(args):
     model = PolynomialFitter(args.polynomial)
+
     if args.method == 'gd':
         dataloader = DataLoader(args.X, args.batch_size, args.polynomial, args.split)
     else:
         dataloader = DataLoader(args.X, -1, args.polynomial, args.split)
-
     train_data_queue = dataloader.get_data_queue(train=True)
     test_data_queue = dataloader.get_data_queue(train=False)
 
     if args.method == 'gd':
         for i in range(args.epochs):
+            avg_loss = AverageMeter()
             for (input_x, target) in train_data_queue:
                 loss = model.train_step(input_x, target, args.lr, args.lamb)
-                print(f'loss {i}: {loss}')
-
+                avg_loss.update(loss, len(input_x))
+            print(f'Epoch {i+1}: {avg_loss.avg}')
     else:
         x,t = train_data_queue[0] 
         model.pinv_method(x, t, args.lamb)
