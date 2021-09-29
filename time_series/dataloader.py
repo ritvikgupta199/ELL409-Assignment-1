cb6142630014d326ecf594260ae3fd287ea66595
Ritvik Gupta <ritvikgupta199@gmail.com>
2021-09-29 22:42:52 +0530

    Completed Time series model

--- time_series/dataloader.py
+++ time_series/dataloader.py
@@ -1,9 +1,10 @@
 import numpy as np
 
 class TrainDataLoader:    
-    def __init__(self, file_path):
+    def __init__(self, file_path, deg):
         self.file_path = file_path
-        self.input_x, self.target = self.load_data()
+        self.deg = deg
+        self.x_mon, self.target = self.load_data()
         self.mu, self.sigma = self.normalise_data()
 
     def load_data(self):
@@ -16,19 +17,41 @@ class TrainDataLoader:
             target[x-1].append(float(data[1]))
         f.close()
         target = np.array([np.array(t).mean() for t in target])
-        input_x = []
+        x_mon = []
         for i in range(1,13):
-            x = float(i)**np.arange(4)
-            input_x.append(x)
-        input_x = np.array(input_x)
-        return input_x, target
+            x = float(i)**np.arange(self.deg+1)
+            x_mon.append(x)
+        x_mon = np.array(x_mon)
+        return x_mon, target
     
     def normalise_data(self):
-        mu = self.input_x.mean(0)
-        sigma = np.std((self.input_x-mu), axis=0)
+        mu = self.x_mon.mean(0)
+        sigma = np.std((self.x_mon-mu), axis=0)
         mu[0], sigma[0] = 0.0, 1.0
-        self.input_x = (self.input_x-mu)/sigma
+        self.x_mon = (self.x_mon-mu)/sigma
         return mu, sigma
 
     def get_data_queue(self):
-        return self.input_x, self.target
+        return self.x_mon, self.target
+
+class TestDataLoader:
+    def __init__(self, file_path, deg):
+        self.file_path = file_path
+        self.deg = deg
+        self.x_mon = self.load_data()
+
+    def load_data(self):
+        f = open(self.file_path, 'r')
+        lines = f.readlines()[1:]
+        x_mon = []
+        for line in lines:
+            data = line.strip().split(',')
+            x = float(data[0].split('/')[0])
+            x = x**np.arange(self.deg+1)
+            x_mon.append(x)
+        x_mon = np.array(x_mon)
+        f.close()
+        return x_mon
+
+    def get_data_queue(self):
+        return self.x_mon
--- time_series/main.py
+++ time_series/main.py
@@ -1,75 +1,62 @@
 import argparse
 import numpy as np
-from dataloader import TrainDataLoader
+from dataloader import TrainDataLoader, TestDataLoader
 from model import LinearModel
 
 def setup():
     parser = argparse.ArgumentParser()
     parser.add_argument("--method", default="pinv", help = "Type of solver")  
     parser.add_argument("--lr", default=0.01, type=float, help = "Learning Rate for gradient descent")
-    parser.add_argument("--epochs", default=100, type=int, help = "Number of epochs")
+    parser.add_argument("--epochs", default=10000, type=int, help = "Number of epochs")
     parser.add_argument("--lamb", default=0, type=float, help = "Regularization constant")
-    parser.add_argument("--result", default="results_2/submit.csv", type=str, help = "Files to store plots")  
+    parser.add_argument("--in_features", default=8, type=int, help = "Number of in features to consider")
+    parser.add_argument("--result", default="results_2/submit.csv", type=str, help = "Files to store plots") 
+    parser.add_argument("--log_wts", default="results_2/log_wts.txt", type=str, help = "Files to store weights")  
     parser.add_argument("--train_data", default="data/train.csv", type=str, help = "Training data file")
     parser.add_argument("--test_data", default="data/test.csv", type=str, help = "Testing data file")
     return parser.parse_args()
 
-class AverageMeter(object):
-  def __init__(self):
-    self.reset()
-
-  def reset(self):
-    self.avg = 0
-    self.sum = 0
-    self.cnt = 0
-
-  def update(self, val, n=1):
-    self.sum += val * n
-    self.cnt += n
-    self.avg = self.sum / self.cnt
-
-
 if __name__ == '__main__':
     args = setup()
     print(args)
-    in_features = 4
 
     if args.method == 'pinv':
         args.batch_size = -1
-    train_data = TrainDataLoader(args.train_data)
-    # test_data = DataLoader(args.test_data, args.batch_size)
+    train_data = TrainDataLoader(args.train_data, args.in_features-1)
+    test_data = TestDataLoader(args.test_data, args.in_features-1)
 
     train_data_queue = train_data.get_data_queue()
-    # test_data_queue = test_data.get_data_queue()
+    test_data_queue = test_data.get_data_queue()
 
-    model = LinearModel(in_features, train_data.mu, train_data.sigma)
+    model = LinearModel(args.in_features, train_data.mu, train_data.sigma)
 
     if args.method == 'gd':
         for i in range(args.epochs):
             input_x, target = train_data_queue
             loss = model.train_step(input_x, target, args.lr, args.lamb)
-            print(f'Epoch {i+1}: Train Loss {loss}')
+            print(f'Epoch {i+1}: Training Loss {loss}')
     else:
         x, t = train_data_queue
         model.pinv_method(x, t, args.lamb)
         y, loss = model.forward(x, t)
         print(f'Training Loss: {loss}')
 
-    # y = []
-    # for (input_x, _) in test_data_queue:
-    #     y.append(model.get_preds(input_x, train_data.mu, train_data.sigma))
-    # y = np.concatenate(y, axis=0)
-    # print(f'Output: {y}')
+    x_test = test_data_queue
+    y = model.get_preds(x_test, train_data.mu, train_data.sigma)
+    print(f'Output: {y}')
 
     weights = model.get_weights(train_data.mu, train_data.sigma)
+    fw = open(args.log_wts, 'w')
+    fw.write(','.join([str(w) for w in weights]))
     print(f'Weights: {weights}')
+    print(f'Weights written in file {args.log_wts}')
 
-    # fw = open(args.result, 'w')
-    # fr = open(args.test_data, 'r')
-    # fw.write('id,value\n')
-    # lines = fr.readlines()[1:]
-    # for (in_x, out_y) in zip(lines, y):
-    #     fw.write(f'{in_x.strip()},{out_y}\n')
-    # fr.close()
-    # fw.close()
-    # print(f'Output written in file {args.result}')
+    fw = open(args.result, 'w')
+    fr = open(args.test_data, 'r')
+    fw.write('id,value\n')
+    lines = fr.readlines()[1:]
+    for (in_x, out_y) in zip(lines, y):
+        fw.write(f'{in_x.strip()},{out_y}\n')
+    fr.close()
+    fw.close()
+    print(f'Output written in file {args.result}')
